f085cfd573ee4f894e3d4d0955e6edc7c39f0688
mcolligan <mcolligan1@gmail.com>
2020-01-14 12:20:09 -0700

    chart load successgit add .

--- client/components/chart.jsx
+++ client/components/chart.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
+import ChartLines from './chartLines.jsx';
 
 class Chart extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      day: [],
+      close: [],
+      loaded: false
     }
   }
 
   componentDidMount() {
     fetch('/chart').then(res => res.json())
-    .then((data) => {
-      console.log(data);
-    })
+      .then((data) => {
+        this.setState({
+          day: Object.keys(data.bpi),
+          close: Object.values(data.bpi),
+          loaded: true
+        })
+      })
   }
 
   render() {
     return (
       <div>
-        <h3>Charts Here</h3>
+        {this.state.loaded &&
+          <div>
+            <h3>Charts Here</h3>
+            <ChartLines info={this.state} />
+          </div>
+        }
       </div>
     )
   }
---
+++ client/components/chartLines.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Chart from 'chart.js';
+
+class ChartLines extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.chartRef = React.createRef();
+  }
+
+
+  componentDidMount() {
+    const myChartRef = this.chartRef.current.getContext("2d");
+    new Chart(myChartRef, {
+      type: "line",
+      data: {
+        labels: [...this.props.info.day],
+        datasets: [
+          {
+          label: "Close Price",
+          data: [...this.props.info.close],
+          }
+        ]
+      },
+      options: {
+
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <canvas id="myChart" ref={this.chartRef}/>
+      </div>
+    )
+  }
+
+}
+
+export default ChartLines;
